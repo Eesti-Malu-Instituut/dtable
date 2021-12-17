d0e5d90afa30884b59e1839c99d6162f5e7b3e7f
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2021-12-17 13:58:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
+
 loadJSON(function(response) {
-  // Parsing JSON string into object
-  var jsondata = JSON.parse(response)
-  console.log(Object.keys(jsondata[0]))
-  var divContainer = document.getElementById("showData")
+  console.log('foo')
+  const jsondata = JSON.parse(response)
+  let persons_container = document.getElementById('showData')
+  let persons_table = document.createElement('table')
+  persons_container.appendChild(persons_table)
+  persons_table.id = 'persons_table'
+  let table_header = persons_table.createTHead()
+  let header_row = table_header.insertRow(0)
+  for (label in jsondata[0]) {
+    let header_cell = header_row.insertCell(0)
+    header_cell.innerText = label
+  }
+  let table_body = persons_table.createTBody()
+
   jsondata.forEach(person => {
-      console.log(person)
-//      Object.keys(person).forEach(field => {
-//          console.log(field)
-//      })
+    console.log(person)
+    let person_row = table_body.insertRow(0)
+    person_row.id = person.jrk
+    // delete(person.jrk)
+    for(label in person) {
+      let cell = person_row.insertCell(0)
+      cell.innerText = person[label]
+    }
   })
   return
 
@@ -69,11 +84,13 @@ function pagination()
 
 function loadJSON(callback) {   
   var xobj = new XMLHttpRequest()
-    xobj.overrideMimeType("application/json")
-    xobj.open('GET', 'echo.json', true)
-    xobj.onreadystatechange = function () {
-      if (xobj.readyState === 4 && xobj.status === "200") {
-        callback(xobj.responseText)
+  xobj.overrideMimeType("application/json")
+  xobj.open('GET', './echo.json', true)
+  xobj.onreadystatechange = function () {
+    // console.log(xobj.readyState, xobj.status)
+    if (xobj.readyState === 4 && xobj.status === 200) {
+      // console.log(xobj.readyState, xobj.status)
+      callback(xobj.responseText)
     }
   }
   xobj.send(null)
