f2b9a57b54aae9256539994375768a6bdc054a5d
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-01-05 10:43:27 +0200

    päisejutt

--- load.js
+++ load.js
@@ -3,13 +3,16 @@ const urlParams = new URLSearchParams(query_string)
 const data_name = urlParams.get('data')
 
 Papa.parse('./data/echo_' + data_name + '.csv', {
-  header: true,
+  header: false,
   download: true,
   dynamicTyping: false,
   complete: function(results) {
     const jsondata = results.data
+    const header_rows = 3
 
     let persons_container = document.getElementById('showData')
+    let description_container = document.getElementById('description')
+    description_container.innerText = jsondata[header_rows-1][1]
     let persons_table = document.createElement('table')
     persons_container.appendChild(persons_table)
     persons_table.id = 'persons_table'
@@ -17,7 +20,9 @@ Papa.parse('./data/echo_' + data_name + '.csv', {
     let table_header = persons_table.createTHead()
     let header_row = table_header.insertRow(0)
     let number_of_cols = 0
-    for (label in jsondata[0]) {
+
+    let labels = jsondata[header_rows]
+    for (label of labels) {
       if (label.substr(0, 2) === 'i_') {
         continue
       }
@@ -28,7 +33,13 @@ Papa.parse('./data/echo_' + data_name + '.csv', {
     }
     let table_body = persons_table.createTBody()
   
+    let header_row_counter = 0
     jsondata.forEach(person => {
+      if(header_row_counter <= header_rows) {
+        header_row_counter ++
+        return
+      }
+
       let person_row = table_body.insertRow()
       person_row.onclick = () => {toggle_row(person.kirjekood)}
       person_row.id = person.kirjekood
